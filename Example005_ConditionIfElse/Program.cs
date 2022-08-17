bfa778becf1340f087a4543f4a8ce5d37cd7f08f
TitovDmitriy <titov-dp@yandex.ru>
2022-08-17 23:33:41 +0300

    Example005 Condition If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "john")
+{
+    Console.WriteLine("Urah, this is John!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
