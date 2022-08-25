97e256d58490bb134af203166d616e4cc2046e8e
TitovDmitriy <titov-dp@yandex.ru>
2022-08-25 17:13:42 +0300

    Вторая лекция: функции и массивы

---
+++ Example007_Function/Program.cs
@@ -0,0 +1,30 @@
+﻿//функция, которая определяет максимальное число
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 321;
+int b1 = 5235;
+int c1 = 112584;
+int a2 = 378;
+int b2 = 9515;
+int c2 = 26;
+int a3 = 657;
+int b3 = 554;
+int c3 = 249;
+
+/*int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
+int max = Max(max1, max2, max3);*/
+
+int max = Max(
+    Max(a1, b1, c1),
+    Max(a2, b2, c2),
+    Max(a3, b3, c3));
+
+Console.WriteLine(max);
---
+++ Example010_Array_Library/Program.cs
@@ -0,0 +1,51 @@
+﻿void FillArray(int[] collection)//метод void не возвращает значение - слово return не пишем
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length; // кол-во эл-ов
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);//выводим значение текущего элемента
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1; //отрицательное значение на случай, если поиск не даст результатов
+
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break; //на случай генерации нескольких одинаковых элементов
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10]; //создай новый массив, в котором будет 10 элементов. 
+
+FillArray(array);//метод заполняет массив элементами
+array[3] = 4;//добавляем несколько одинаковых эл-в
+array[6] = 4; //чтобы посмотреть, как метод поиска себя проявит
+
+PrintArray(array);// метод распечатывает
+Console.WriteLine(); //пустая строка, чтобы отграничить вывод массива
+
+int pos = IndexOf(array, 4);// ищем эл-т равный 4, просто из любопытства
+Console.WriteLine(pos);
